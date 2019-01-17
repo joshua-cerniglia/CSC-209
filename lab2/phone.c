da32d88155cbe3b498928a49d6370cff27537a5c
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2019-01-17 16:15:55 -0500

    Fixed

---
+++ lab2/phone.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  char phone[11];
+  int part2 = 0;
+  scanf("%s %d", phone, &part2);
+  if (part2 == -1){
+    printf("%s\n", phone);
+  }
+  else if (part2 <= 9 && part2 >= 0){
+    printf("%c\n", phone[part2]);
+  }
+  else{
+    printf("ERROR");
+  }
+  return 0;
+}
