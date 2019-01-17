b22bec015f2ea1e60a2d53b4079f6d8b64552fec
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2019-01-17 16:57:10 -0500

    Created

---
+++ lab2/phone_loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char **argv) {
+  char phone[11];
+  int part2 = 0;
+  scanf("%s", phone);
+  while(5<10){
+  scanf("%d", &part2);
+    if (part2 == -1){
+      printf("%s\n", phone);
+    }
+    else if (part2 <= 9 && part2 >= 0){
+      printf("%c\n", phone[part2]);
+    }
+    else{
+      printf("ERROR\n");
+    }
+  }
+  return 0;
+}
