c812dbcc566905ebc058804b393b1931075ee9f0
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2019-01-31 00:25:25 -0500

    Started

--- a1/trim.c
+++ a1/trim.c
@@ -8,7 +8,7 @@
  */
 
 int main(int argc, char **argv) {
-    
+
     if(argc != 3) {
          fprintf(stderr, "Usage: %s tracefile markerfile\n", argv[0]);
          exit(1);
@@ -16,7 +16,9 @@ int main(int argc, char **argv) {
 
     // Addresses should be stored in unsigned long variables
     // unsigned long start_marker, end_marker;
-
+    unsigned long start_marker = 0;
+    unsigned long end_marker = 0;
+    fscanf(argv[2], "%lu,%lu", start_marker, end_marker);
 
 
     /* For printing output, use this exact formatting string where the
@@ -24,6 +26,19 @@ int main(int argc, char **argv) {
      * is the address
      */
     // printf("%c,%#lx\n", VARIABLES TO PRINT GO HERE);
-
+    int bool = 0;
+    char name = 'F';
+    unsigned long hex = 0;
+    while(fscanf(argv[1], "%c,%lu", name, hex) == 1){
+      if(hex == start_marker){
+        bool = 1;
+      }
+      if(bool == 1){
+        printf("%c,%#lx\n", name, hex);
+      }
+      if(hex == end_marker){
+        bool = 0;
+      }
+    }
     return 0;
 }
