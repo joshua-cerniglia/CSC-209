da3db1fe34c69cbe9b8cd0afe8abad4d76340353
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2019-01-31 03:46:34 -0500

    Testing

--- a1/trim.c
+++ a1/trim.c
@@ -32,6 +32,9 @@ int main(int argc, char **argv) {
     int bool = 0;
     char name = 'F';
     unsigned long hex = 0;
+    if(trace == NULL){
+      printf("hi");
+    }
     trace = fopen(argv[1], "r");
     if(trace == NULL){
       printf("hi");
